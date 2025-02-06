2d4b9f5288bb57c0cf387f6c04314b342bceff8e
pedroaal <pedroaal04@gmail.com>
2025-02-06 13:43:22 -0500

    added user migration

---
+++ drizzle/0001_gifted_captain_britain.sql
@@ -0,0 +1,10 @@
+CREATE TABLE "users" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "users_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"first_name" varchar(255) NOT NULL,
+	"last_name" varchar(255) NOT NULL,
+	"email" text NOT NULL,
+	"password" text NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL,
+	"deleted_at" timestamp
+);
