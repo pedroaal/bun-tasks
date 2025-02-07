c622e97c17f4df20b7431c0048c2737689b6e9b7
pedroaal <pedroaal04@gmail.com>
2025-02-07 09:09:25 -0500

    update base migration

--- drizzle/0000_married_morbius.sql
+++ drizzle/0000_married_morbius.sql
@@ -1,9 +1,21 @@
 CREATE TYPE "public"."tasks_status" AS ENUM('CREATED', 'COMPLETED');--> statement-breakpoint
 CREATE TABLE "tasks" (
 	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "tasks_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"user_id" integer NOT NULL,
 	"title" varchar(255) NOT NULL,
 	"description" varchar(255) NOT NULL,
 	"status" "tasks_status" DEFAULT 'CREATED',
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL,
+	"deleted_at" timestamp
+);
+--> statement-breakpoint
+CREATE TABLE "users" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "users_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"first_name" varchar(255) NOT NULL,
+	"last_name" varchar(255) NOT NULL,
+	"email" text NOT NULL,
+	"password" text NOT NULL,
 	"created_at" timestamp DEFAULT now() NOT NULL,
 	"updated_at" timestamp DEFAULT now() NOT NULL,
 	"deleted_at" timestamp
