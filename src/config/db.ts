fd07b2e51567873097b68e06be4ad3259a3c84cd
Pedro Andrés Altamirano López <pedroaal04@gmail.com>
2025-03-25 00:35:54 +0000

    to sqlite

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,6 +1,7 @@
-import { drizzle } from "drizzle-orm/bun-sql"
+import { drizzle } from "drizzle-orm/bun-sqlite";
+import { Database } from "bun:sqlite";
 
-const DATABASE_URL = process.env.DATABASE_URL ?? ""
-const db = drizzle(DATABASE_URL, { casing: "snake_case" })
+const sqlite = new Database("tasks.db");
+export const db = drizzle(sqlite, { casing: "snake_case" });
 
-export default db
+export default db;
