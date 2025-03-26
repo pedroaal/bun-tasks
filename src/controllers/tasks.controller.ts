8ca19eee2c3c5996196379c24d9e240790b1b599
pedroaal <pedroaal04@gmail.com>
2025-03-25 21:58:22 -0500

    remove definitions

--- src/controllers/tasks.controller.ts
+++ src/controllers/tasks.controller.ts
@@ -1,44 +1,33 @@
-import { Hono } from "hono";
-import { zValidator } from "@hono/zod-validator";
+import { Hono } from "hono"
+import { zValidator } from "@hono/zod-validator"
 
-import TaskDao from "../services/task.dao";
+import TaskDao from "../services/task.dao"
 import {
   CreateTaskSchema,
   UpdateTaskSchema,
-} from "../database/tasks.validation";
+} from "../database/tasks.validation"
 
-const TaskRouter = new Hono().basePath("/task");
-const repo = new TaskDao();
+const TaskRouter = new Hono().basePath("/task")
+const repo = new TaskDao()
 
 TaskRouter.get("/", async (ctx) => {
-  const tasks = await repo.findAll();
-  return ctx.json(tasks);
-});
+  return ctx.json(undefined)
+})
 
 TaskRouter.post("/", zValidator("json", CreateTaskSchema), async (ctx) => {
-  const body = ctx.req.valid("json");
-  const task = await repo.create({ ...body });
-  return ctx.json(task);
-});
+  return ctx.json(undefined)
+})
 
 TaskRouter.get("/:id", async (ctx) => {
-  const id = ctx.req.param("id");
-  const task = await repo.findById(+id);
-  return ctx.json(task);
-});
+  return ctx.json(undefined)
+})
 
 TaskRouter.patch("/:id", zValidator("json", UpdateTaskSchema), async (ctx) => {
-  const id = ctx.req.param("id");
-  const body = ctx.req.valid("json");
-  const task = await repo.update(+id, body);
-  return ctx.json(task);
-});
+  return ctx.json(undefined)
+})
 
 TaskRouter.delete("/:id", async (ctx) => {
-  const id = ctx.req.param("id");
-  const task = await repo.findById(+id);
-  await repo.delete(+id);
-  return;
-});
+  return
+})
 
-export default TaskRouter;
+export default TaskRouter
